678a138e0b9f4bfc183267e0f5d56061193ef9c6
mnilcl@naver.com <mnilcl@naver.com>
2018-11-25 23:46:41 +0900

    init jekyll site

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Welcome to jemijin",
+        "excerpt":"Minimal Mistakes Jekyll Theme Minimal Mistakes is a flexible two-column Jekyll theme. Perfect for hosting your personal site, blog, or portfolio on GitHub or self-hosting on your own server. As the name implies – styling is purposely minimalistic to be enhanced and customized by you :smile:. :sparkles: See what’s new...","categories": ["Hello"],
+        "tags": ["hello"],
+        "url": "http://0.0.0.0/hello/init/",
+        "teaser":null}]
---
+++ run.sh
@@ -0,0 +1,1 @@
+sudo jekyll serve --livereload --host 0.0.0.0 --port 80
